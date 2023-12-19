9c2068ab52468d904c350180256192e55153648a
Inderby <youk8253@gmail.com>
2023-12-19 17:34:05 +0900

    add login form

--- src/component/LoginComponent.jsx
+++ src/component/LoginComponent.jsx
@@ -1,7 +1,19 @@
 export default function LoginComponent(){
     return (
         <div className="Login">
-            Login Component
+            <div className="LoginForm">
+                <div>
+                    <lable>User Name</lable>
+                    <input type="text" name="username"/>
+                </div>
+                <div>
+                 <lable>Password</lable>
+                    <input type="password" name="password"/>
+                </div>
+                <div>
+                    <button type="button" name="login">login</button>
+                </div>
+            </div>
         </div>
     )
 }

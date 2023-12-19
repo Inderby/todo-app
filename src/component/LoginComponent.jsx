a8a5e0a8f246dc758778bdf1c24e41079f32f360
Inderby <youk8253@gmail.com>
2023-12-19 19:09:34 +0900

    login logic hard coding(will be refactoring later)

--- src/component/LoginComponent.jsx
+++ src/component/LoginComponent.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 import './LoginComponent.css'
 export default function LoginComponent() {
 
-    const [username, setUsername] = useState('in28minutes')
+    const [username, setUsername] = useState('foo')
     const [password, setPassword] = useState('')
-
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
     function handleUsernameChange(event) {
         console.log(event.target.value)
         setUsername(event.target.value)
@@ -14,8 +15,20 @@ export default function LoginComponent() {
         setPassword(event.target.value)
     }
 
+    function handleSubmit() {
+        if (username === 'foo' && password === 'dummy') {
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+        } else {
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+        }
+    }
+
     return (
         <div className="Login">
+            {showSuccessMessage && <div className="successMessage">Authenicated Successfully</div>}
+            {showErrorMessage && <div className="errorMessage">Authenicated Failed. Please check your credentials</div>}
             <div className="LoginForm">
                 <div>
                     <lable>User Name:</lable>
@@ -26,9 +39,11 @@ export default function LoginComponent() {
                     <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <div>    
-                    <button type="button" name="login">login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>login</button>
                 </div>
             </div>
         </div>
     )
 }
+
+

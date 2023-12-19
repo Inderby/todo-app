d90aeeab8d303407e7d6c5ca3e5e8a9110de6431
Inderby <youk8253@gmail.com>
2023-12-19 20:40:29 +0900

    add todo page flow

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,6 +1,7 @@
 
 import WelcomeComponent from "./WelcomeComponent"
 import LoginComponent from "./LoginComponent"
+import ListTodosComponent from "./ListTodosComponent"
 import { BrowserRouter, Routes, Route , useParams} from "react-router-dom"
 import ErrorComponent from "./ErrorComponent"
 export default function TodoApp(){
@@ -11,7 +12,8 @@ export default function TodoApp(){
                     <Route path="/" element={<LoginComponent/>}></Route>
                     <Route path="/login" element={<LoginComponent/>}></Route>
                     <Route path="/welcome/:username" element={<WelcomeComponent />}></Route>
-                    <Route path="*" element={<ErrorComponent/>}></Route>
+                    <Route path="/todos" element={<ListTodosComponent/>}></Route>
+                    <Route path="*" element={<ErrorComponent />}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
--- src/component/WelcomeComponent.jsx
+++ src/component/WelcomeComponent.jsx
@@ -1,11 +1,11 @@
-import { useParams } from "react-router-dom"
+import { useParams, Link} from "react-router-dom"
 export default function WelcomeComponent() {
     const param = useParams()
     return (
         <div className="WelcomeComponent">
             <h1>Welcome {param.username}</h1>
             <div>
-                welcome conponent
+                Manage your todos. <Link to="/todos">Go here</Link>
             </div>
         </div>
     )

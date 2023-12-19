031e330000f31bd47b0d4817e28b2b104d96592a
Inderby <youk8253@gmail.com>
2023-12-19 18:40:30 +0900

    add state `username`, `password`

--- src/component/LoginComponent.jsx
+++ src/component/LoginComponent.jsx
@@ -1,16 +1,31 @@
-export default function LoginComponent(){
+import { useState } from 'react'
+import './LoginComponent.css'
+export default function LoginComponent() {
+
+    const [username, setUsername] = useState('in28minutes')
+    const [password, setPassword] = useState('')
+
+    function handleUsernameChange(event) {
+        console.log(event.target.value)
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
-                    <lable>User Name</lable>
-                    <input type="text" name="username"/>
+                    <lable>User Name:</lable>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
                  <lable>Password</lable>
-                    <input type="password" name="password"/>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
-                <div>
+                <div>    
                     <button type="button" name="login">login</button>
                 </div>
             </div>

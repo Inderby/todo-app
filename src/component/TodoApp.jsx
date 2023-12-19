1627e66348fbb972e33b455b7da834baa873953c
Inderby <youk8253@gmail.com>
2023-12-19 20:53:33 +0900

    add sub components

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -4,18 +4,24 @@ import LoginComponent from "./LoginComponent"
 import ListTodosComponent from "./ListTodosComponent"
 import { BrowserRouter, Routes, Route , useParams} from "react-router-dom"
 import ErrorComponent from "./ErrorComponent"
+import HeaderComponent from "./HeaderComponent"
+import FooterComponent from "./FooterComponent"
+import LogoutComponent from "./LogoutComponent"
 export default function TodoApp(){
     return (
         <div className="TodoApp">
+            <HeaderComponent/>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<LoginComponent/>}></Route>
                     <Route path="/login" element={<LoginComponent/>}></Route>
                     <Route path="/welcome/:username" element={<WelcomeComponent />}></Route>
-                    <Route path="/todos" element={<ListTodosComponent/>}></Route>
+                    <Route path="/todos" element={<ListTodosComponent />}></Route>
+                    <Route path="/logout" element={<LogoutComponent/>}></Route>
                     <Route path="*" element={<ErrorComponent />}></Route>
                 </Routes>
             </BrowserRouter>
+            <FooterComponent/>
         </div>
     )
 }

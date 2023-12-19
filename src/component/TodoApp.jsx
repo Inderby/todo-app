7d68b52dda3903aca9eda64f5025e2a3133fd7f7
Inderby <youk8253@gmail.com>
2023-12-19 19:36:20 +0900

    generate `ErrorComponent`

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,7 +1,8 @@
 
 import WelcomeComponent from "./WelcomeComponent"
 import LoginComponent from "./LoginComponent"
-import { BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import ErrorComponent from "./ErrorComponent"
 export default function TodoApp(){
     return (
         <div className="TodoApp">
@@ -9,7 +10,8 @@ export default function TodoApp(){
                 <Routes>
                     <Route path="/" element={<LoginComponent/>}></Route>
                     <Route path="/login" element={<LoginComponent/>}></Route>
-                    <Route path="/welcome" element={<WelcomeComponent/>}></Route>
+                    <Route path="/welcome" element={<WelcomeComponent />}></Route>
+                    <Route path="*" element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>

6fb8139a0a5d2080de8ec302f9df8e1b66383a24
Inderby <youk8253@gmail.com>
2023-12-19 16:46:48 +0900

    Generate `WelcomeComponent.jsx`

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,7 +1,9 @@
+import WelcomeComponent from "./WelcomeComponent"
 export default function TodoApp(){
     return (
         <div className="TodoApp">
             Todo Management Application
+            <WelcomeComponent/>
         </div>
     )
 }

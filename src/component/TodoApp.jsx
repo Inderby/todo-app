4a426329b992ece8bf6793c713c9c81c5c543d05
Inderby <youk8253@gmail.com>
2023-12-19 16:41:41 +0900

    generate `LoginComponent.jsx`

--- src/component/TodoApp.jsx
+++ src/component/TodoApp.jsx
@@ -1,7 +1,10 @@
+import LoginComponent from "./LoginComponent"
 export default function TodoApp(){
     return (
         <div className="TodoApp">
             Todo Management Application
+            <LoginComponent/>
         </div>
     )
 }
+

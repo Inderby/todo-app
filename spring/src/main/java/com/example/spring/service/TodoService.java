5df8951adbb720816c0c10850d234f1230d556d9
Inderby <youk8253@gmail.com>
2023-12-20 20:19:42 +0900

    generate `TodoRepository` class

---
+++ spring/src/main/java/com/example/spring/service/TodoService.java
@@ -0,0 +1,23 @@
+package com.example.spring.service;
+
+import com.example.spring.dto.Todo;
+import com.example.spring.repository.TodoRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequiredArgsConstructor
+public class TodoService {
+
+  private final TodoRepository todoRepository;
+
+  public List<Todo> getTodoList(String username) {
+    return null;
+  }
+
+  public List<Todo> findByUsername(String username) {
+    return todoRepository.findByUsername(username);
+  }
+}

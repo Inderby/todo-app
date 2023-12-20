3dda72627b633ecf9143807fd6848a16cbc46b34
Inderby <youk8253@gmail.com>
2023-12-20 20:18:02 +0900

    generate dto `Todo` class

---
+++ spring/src/main/java/com/example/spring/dto/Todo.java
@@ -0,0 +1,28 @@
+package com.example.spring.dto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Todo {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String username;
+  private String description;
+  private Date targetDate;
+  private boolean done;
+}

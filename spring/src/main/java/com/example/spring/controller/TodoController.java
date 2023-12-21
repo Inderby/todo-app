6a1f5a6b5d4b9d050ac60890e19fc5f6bbbe1da1
Inderby <youk8253@gmail.com>
2023-12-21 12:32:39 +0900

    implment controller to retrieve todos

--- spring/src/main/java/com/example/spring/controller/TodoController.java
+++ spring/src/main/java/com/example/spring/controller/TodoController.java
@@ -2,9 +2,9 @@ package com.example.spring.controller;
 
 import com.example.spring.dto.Todo;
 import com.example.spring.service.TodoService;
-
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +13,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class TodoController {
-    public TodoService todoService;
 
-    public TodoController(TodoService todoService){
-        this.todoService = todoService;
-    }
+  public TodoService todoService;
 
-    @GetMapping("/users/{username}/todos")
-    public List<Todo> retrieveTodos(@PathVariable String username){
-      return todoService.findByUsername(username);
-    }
+  public TodoController(TodoService todoService) {
+    this.todoService = todoService;
+  }
+
+  @GetMapping("users/{username}/todos")
+  public List<Todo> retrieveTodos(
+    @PathVariable(name = "username") String username
+  ) {
+    // return todoService.findByUsername(username);
+    Todo todo1 = new Todo(
+      Long.valueOf(1),
+      "test",
+      "test1",
+      LocalDate.now(),
+      false
+    );
+    Todo todo2 = new Todo(
+      Long.valueOf(2),
+      "test",
+      "test2",
+      LocalDate.now(),
+      false
+    );
+    Todo todo3 = new Todo(
+      Long.valueOf(3),
+      "test",
+      "test3",
+      LocalDate.now(),
+      false
+    );
+    return List.of(todo1, todo2, todo3);
+  }
 }

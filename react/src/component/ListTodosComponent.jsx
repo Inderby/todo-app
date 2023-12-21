eae42c23e6aba2b18eb9d6d4398d3e9903554511
Inderby <youk8253@gmail.com>
2023-12-21 12:33:33 +0900

    add feature that request todos from rest api

--- react/src/component/ListTodosComponent.jsx
+++ react/src/component/ListTodosComponent.jsx
@@ -1,40 +1,55 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodoForUsername } from "./api/TodoApi";
+
 export default function ListTodosComponent() {
-    const today = new Date()
-    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
-    const todos = [
-        { id: 1, description: 'Learn AWS', done: false, targetDate: targetDate},
-        { id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate },
-        { id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate },
-    ]
-    return (
-        <div className="ListTodosComponent">
-            <h1>Things You Want To Do!</h1>
-            <div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <td>ID</td>
-                            <td>description</td>
-                            <td>is Done?</td>
-                            <td>Target Date</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            todos.map(
-                                todo =>(
-                                    <tr key={todo.id}>
-                                        <td>{todo.id}</td>
-                                        <td>{todo.description}</td>
-                                        <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toDateString()}</td>
-                                    </tr>
-                                )
-                            )
-                        }
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    )
+  const today = new Date();
+  const targetDate = new Date(
+    today.getFullYear() + 12,
+    today.getMonth(),
+    today.getDay()
+  );
+
+  const [todos, setTodos] = useState([]);
+  // const todos = [
+  //     { id: 1, description: 'Learn AWS', done: false, targetDate: targetDate},
+  //     { id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate },
+  //     { id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate },
+  // ]
+
+  useEffect(() => refreshTodos(), []);
+
+  function refreshTodos() {
+    retrieveAllTodoForUsername("test")
+      .then((response) => {
+        setTodos(response.data);
+      })
+      .catch((error) => console.log(error));
+  }
+  return (
+    <div className="ListTodosComponent">
+      <h1>Things You Want To Do!</h1>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>description</td>
+              <td>is Done?</td>
+              <td>Target Date</td>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo) => (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.description}</td>
+                <td>{todo.done.toString()}</td>
+                <td>{todo.targetDate.toString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }

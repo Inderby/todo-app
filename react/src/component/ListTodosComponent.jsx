95bf2e265902332c7774daccf54486e23d562e41
Inderby <youk8253@gmail.com>
2023-12-21 18:27:50 +0900

    implement update and create view and feature

--- react/src/component/ListTodosComponent.jsx
+++ react/src/component/ListTodosComponent.jsx
@@ -38,6 +38,10 @@ export default function ListTodosComponent() {
     navigate(`/todo/${id}`);
   }
 
+  function addNewTodo() {
+    navigate(`/todo/-1`);
+  }
+
   return (
     <div className="ListTodosComponent">
       <h1>Things You Want To Do!</h1>
@@ -78,6 +82,9 @@ export default function ListTodosComponent() {
             ))}
           </tbody>
         </table>
+      </div>
+      <div className="btn btn-success m-5" onClick={addNewTodo}>
+        Add New Todo
       </div>
     </div>
   );

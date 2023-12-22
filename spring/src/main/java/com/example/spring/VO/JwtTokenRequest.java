83f324f4ba46ce45415df4608b10a552f56f34a0
Inderby <youk8253@gmail.com>
2023-12-22 18:31:21 +0900

    implement jwt token generate and autherized feature

---
+++ spring/src/main/java/com/example/spring/VO/JwtTokenRequest.java
@@ -0,0 +1,3 @@
+package com.example.spring.VO;
+
+public record JwtTokenRequest(String username, String password) {}
---
+++ spring/src/main/java/com/example/spring/VO/JwtTokenResponse.java
@@ -0,0 +1,3 @@
+package com.example.spring.VO;
+
+public record JwtTokenResponse(String token) {}

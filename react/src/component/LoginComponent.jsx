74db1f642871ee8b2900fcbfe6f4868e2fda6e47
Inderby <youk8253@gmail.com>
2023-12-22 18:29:10 +0900

    add keyword `async`, `await` for login

--- react/src/component/LoginComponent.jsx
+++ react/src/component/LoginComponent.jsx
@@ -16,8 +16,8 @@ export default function LoginComponent() {
     setPassword(event.target.value);
   }
 
-  function handleSubmit() {
-    if (authContext.login(username, password)) {
+  async function handleSubmit() {
+    if (await authContext.login(username, password)) {
       authContext.setUsername(username);
       navigate(`/welcome/${username}`);
     } else {

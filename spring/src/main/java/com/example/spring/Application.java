dd23d9818c5e398dc6bbeecfed4588f1a0168d3f
Inderby <youk8253@gmail.com>
2023-12-22 18:22:41 +0900

    add annoation to scan package

--- spring/src/main/java/com/example/spring/Application.java
+++ spring/src/main/java/com/example/spring/Application.java
@@ -3,10 +3,12 @@ package com.example.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.example.spring.configure" })
 public class Application {
 
   public static void main(String[] args) {

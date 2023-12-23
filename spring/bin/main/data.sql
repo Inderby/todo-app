f7e6a333e45f1d322a1cb9dc75f9f468219d9d76
Inderby <youk8253@gmail.com>
2023-12-23 16:17:20 +0900

    add initialize data

---
+++ spring/bin/main/data.sql
@@ -0,0 +1,32 @@
+insert into
+  todo(id, username, description, target_date, done)
+values
+  (
+    default,
+    'user',
+    'full stack app',
+    CURRENT_DATE(),
+    false
+  );
+
+insert into
+  todo(id, username, description, target_date, done)
+values
+  (
+    default,
+    'user',
+    'Learn AWS',
+    CURRENT_DATE(),
+    false
+  );
+
+insert into
+  todo(id, username, description, target_date, done)
+values
+  (
+    default,
+    'user',
+    'learn devops',
+    CURRENT_DATE(),
+    false
+  );
